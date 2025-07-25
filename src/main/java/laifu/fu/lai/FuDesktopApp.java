9fc9d1b8bb1bdfe256eb7ce094f8ba62f3fe47b1
CreamGod45 <fuyin1054@gmail.com>
2025-07-25 11:43:55 +0800

    Refine FuDesktopApp with error handling

--- src/main/java/laifu/fu/lai/FuDesktopApp.java
+++ src/main/java/laifu/fu/lai/FuDesktopApp.java
@@ -1,9 +1,9 @@
 package laifu.fu.lai;
 
 import me.friwi.jcefmaven.CefAppBuilder;
+import me.friwi.jcefmaven.CefInitializationException;
 import me.friwi.jcefmaven.MavenCefAppHandlerAdapter;
 import me.friwi.jcefmaven.UnsupportedPlatformException;
-import me.friwi.jcefmaven.CefInitializationException;
 import org.cef.CefApp;
 import org.cef.CefClient;
 import org.cef.browser.CefBrowser;
@@ -14,9 +14,31 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FuDesktopApp {
-    public static void main(String[] args) throws UnsupportedPlatformException, CefInitializationException, IOException, InterruptedException {
+    private static final Logger LOGGER = Logger.getLogger(FuDesktopApp.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            start(args);
+        } catch (UnsupportedPlatformException | CefInitializationException e) {
+            LOGGER.log(Level.SEVERE, "Failed to initialize platform", e);
+            System.err.println("This platform is not supported or CEF failed to initialize.");
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "I/O error", e);
+            System.err.println("An I/O error occurred: " + e.getMessage());
+        } catch (InterruptedException e) {
+            LOGGER.log(Level.SEVERE, "Application interrupted", e);
+            Thread.currentThread().interrupt();
+            System.err.println("Application startup was interrupted.");
+        }
+    }
+
+    private static void start(String[] args)
+            throws UnsupportedPlatformException, CefInitializationException,
+            IOException, InterruptedException {
         // Start Spring Boot server
         org.springframework.boot.SpringApplication.run(FuApplication.class, args);
 
